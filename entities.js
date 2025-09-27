0b12bad3735eb4951187d10a4ab27b7359bc67d1
TechEqualizer <bigd49812@gmail.com>
2025-09-27 10:33:03 -0400

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,21 @@
+import { base44 } from './base44Client';
+
+
+export const Donation = base44.entities.Donation;
+
+export const KidReferral = base44.entities.KidReferral;
+
+export const GalleryItem = base44.entities.GalleryItem;
+
+export const NewsletterSubscriber = base44.entities.NewsletterSubscriber;
+
+export const Testimonial = base44.entities.Testimonial;
+
+export const FundraisingCampaign = base44.entities.FundraisingCampaign;
+
+export const FundraisingEvent = base44.entities.FundraisingEvent;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ functions.js
@@ -0,0 +1,7 @@
+import { base44 } from './base44Client';
+
+
+export const createStripeCheckout = base44.functions.createStripeCheckout;
+
+export const stripeWebhook = base44.functions.stripeWebhook;
+
