0b8bb398ec4ee5e01451765f70a27653bf29f9dc
TechEqualizer <bigd49812@gmail.com>
2025-09-27 10:49:47 -0400

    Fix Vite import path resolution errors

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { createPageUrl } from "@/utils";
@@ -429,4 +427,3 @@ export default function Layout({ children, currentPageName }) {
     </div>
   );
 }
-
